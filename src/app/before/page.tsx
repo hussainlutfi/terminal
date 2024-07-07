cbf2bf970184483227767087c54e3081535bbb65
7ussainlz <hussainlutfi@hotmail.com>
2024-07-07 10:01:49 +0300

    NextFont

--- src/app/before/page.tsx
+++ src/app/before/page.tsx
@@ -2,10 +2,20 @@
 
 import { QABefore } from "../../../data/before";
 import AccordionList from "../components/accordionList";
+import { Tajawal } from "next/font/google";
+
+const tajawal = Tajawal({
+  subsets: ["latin"],
+  variable: "--font-tajawal",
+  weight: "400",
+});
 
 export default function BeforePage() {
   return (
-    <main className="flex min-h-screen min-w-full">
+    <main
+      dir="rtl"
+      className={`flex min-h-screen min-w-full ${tajawal.className} font-sans`}
+    >
       <div className="w-full bg-gradient-to-br bg-gradient-to-br from-[#51170e] to-[#031020] to-80%">
         <div className="flex flex-col items-start justify-center px-5 pt-12 sm:pt-16">
           <h1 className="text-white text-3xl mb-[45px] sm:text-5xl font-extrabold leading-normal sm:leading-tight tracking-tight text-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import MainSquares from "./components/main-squares";
 import HomeHeader from "./components/home-header";
 import QuestionSquare from "./components/question-input";
+import { Tajawal } from "next/font/google";
+
+const tajawal = Tajawal({
+  subsets: ["latin"],
+  variable: "--font-tajawal",
+  weight: "400",
+});
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-full">
+    <main
+      dir="rtl"
+      className={`flex min-h-screen min-w-full ${tajawal.className} font-sans`}
+    >
       <div className="w-full bg-gradient-to-br from-[#51170e] to-[#031020] to-80%">
         <div className="flex flex-col items-center justify-center pt-16 sm:pt-16">
           <HomeHeader />
